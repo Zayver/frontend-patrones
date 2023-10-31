2fe9fcde84652d4cfd565334682d7880df1f03c5
Zayver <zayber.re@gmail.com>
2023-10-31 14:31:29 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './modules/error/components/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: "home", pathMatch: 'full' },
+  { path: "home", loadChildren: () => import('src/app/modules/home/home.module').then(m => m.HomeModule), title: "Inicio" },
+  { path: "**", loadChildren: () => import('src/app/modules/error/error.module').then(m => m.ErrorModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MessageService } from 'primeng/api';
+import { HttpClientModule } from '@angular/common/http'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
